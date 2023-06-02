86f6fce50bf5f93d8c04032a244e4f580972585a
mickeyhaile2 <108958468+mickeyhaile2@users.noreply.github.com>
2023-06-02 18:15:17 +0100

    update

--- E-Commerce-API/app.js
+++ E-Commerce-API/app.js
@@ -5,7 +5,7 @@ var bodyParser = require("body-parser");
 // Don't hardcode your DB password in the code or upload it to GitHub! Never ever do this ever.
 // Use environment variables instead:
 // https://www.codementor.io/@parthibakumarmurugesan/what-is-env-how-to-set-up-and-run-a-env-file-in-node-1pnyxw9yxj
-
+require("dotenv").config();
 const { Pool } = require("pg");
 
 DB_HOST=localhost
