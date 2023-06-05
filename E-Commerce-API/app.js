f82fb01196cca6202c8c6612c5767e300925198d
Bahare <itxbb09@gmail.com>
2023-06-05 21:50:09 +0100

    a list of all product names with their prices and supplier names

--- E-Commerce-API/app.js
+++ E-Commerce-API/app.js
@@ -1,8 +1,34 @@
 const express = require("express");
+const cors = require("cors");
+const { Pool } = require("pg");
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
-// Your code to run the server should go here
-// Don't hardcode your DB password in the code or upload it to GitHub! Never ever do this ever.
-// Use environment variables instead:
-// https://www.codementor.io/@parthibakumarmurugesan/what-is-env-how-to-set-up-and-run-a-env-file-in-node-1pnyxw9yxj
 
-module.exports = app;
+app.use(cors());
+app.use(express.json());
+
+const db = new Pool({
+  user: process.env.DB_USERNAME,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+//Endpoint `/products` should return a list of all product names with their prices and supplier names.
+app.get("/products", async (req, res) => {
+  try {
+    const result = await db.query(
+      "SELECT p.product_name as name, pa.unit_price as price, s.supplier_name as supplierName FROM products p INNER JOIN product_availability pa on (p.id = pa.prod_id) INNER JOIN suppliers s on (pa.supp_id = s.id)"
+    );
+
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
