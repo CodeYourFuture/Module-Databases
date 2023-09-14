8b0977de19da1aa8799d04d3cf47c21aa83a0be1
christina-mifsud <christina.mifsud@gmail.com>
2023-09-14 21:24:39 +0100

    User story 2 - test and endpoint

--- E-Commerce-API/app.js
+++ E-Commerce-API/app.js
@@ -15,4 +15,33 @@ const db = new Pool({
   port: process.env.DB_PORT,
 });
 
+///////////// What database should I use here?
+
+// 1. As a user, I want to view a list of all products with their prices and supplier names.
+app.get("/products", async function (req, res) {
+  await db
+    .query(
+      "SELECT p.id AS product_id, p.product_name, pa.unit_price, pa.supp_id AS supplier_id FROM products p JOIN product_availability pa ON (p.id = pa.prod_id)"
+    )
+    .then((result) => {
+      res.status(200).json(result.rows);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+// 2. As a user, I want to search for products by name.
+app.get("/products/:name", function (request, response) {
+  const partialName = "%" + request.params.name + "%"; // % to match any part of the name
+  db.query("SELECT * FROM products WHERE product_name LIKE $1", [partialName])
+    .then((result) => {
+      response.status(200).json(result.rows);
+    })
+    .catch((error) => {
+      console.log(error);
+      response.status(500).json(error);
+    });
+});
+
 module.exports = app;
--- E-Commerce-API/tests/products.spec.js
+++ E-Commerce-API/tests/products.spec.js
@@ -1,6 +1,7 @@
 const request = require("supertest");
 const app = require("../app");
 
+// 1. As a user, I want to view a list of all products with their prices and supplier names.
 describe("GET /products", () => {
   it("should return a list of all product names with their prices and supplier names", async () => {
     const response = await request(app).get("/products");
@@ -16,3 +17,21 @@ describe("GET /products", () => {
     );
   });
 });
+
+// 2. As a user, I want to search for products by name.
+describe("GET /products/:name", () => {
+  it("should return a list of all product names which match search query", async () => {
+    const response = await request(app).get("/products/mobile");
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          name: "mobile",
+        }),
+      ])
+    );
+  });
+});
+
+// 3. As a user, I want to view a single customer by their ID.
+// 4. As a user, I want to create a new customer with their name, address, city, and country.
