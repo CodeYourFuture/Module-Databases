9a38d23163dbb4562ff0e5d45609e1a4d19e6369
christina-mifsud <christina.mifsud@gmail.com>
2023-09-15 00:34:09 +0100

    User story 3 - test and endpoint
    Also fixed issues with DB connection

--- E-Commerce-API/app.js
+++ E-Commerce-API/app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+
+require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+
 // Your code to run the server should go here
 // Don't hardcode your DB password in the code or upload it to GitHub! Never ever do this ever.
 // Use environment variables instead:
@@ -8,14 +13,12 @@ const app = express();
 const { Pool } = require("pg");
 
 const db = new Pool({
-  user: process.env.DB_USERNAME,
+  user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 });
-
-///////////// What database should I use here?
 
 // 1. As a user, I want to view a list of all products with their prices and supplier names.
 app.get("/products", async function (req, res) {
@@ -34,7 +37,7 @@ app.get("/products", async function (req, res) {
 // 2. As a user, I want to search for products by name.
 app.get("/products/:name", function (request, response) {
   const partialName = "%" + request.params.name + "%"; // % to match any part of the name
-  db.query("SELECT * FROM products WHERE product_name LIKE $1", [partialName])
+  db.query("SELECT * FROM products WHERE product_name ILIKE $1", [partialName])
     .then((result) => {
       response.status(200).json(result.rows);
     })
@@ -42,6 +45,22 @@ app.get("/products/:name", function (request, response) {
       console.log(error);
       response.status(500).json(error);
     });
+});
+
+// 3. As a user, I want to view a single customer by their ID.
+app.get("/customers/:customerId", function (request, response) {
+  const customerId = parseInt(request.params.customerId);
+  db.query("SELECT * FROM customers WHERE id = $1", [customerId])
+    .then((result) => {
+      response.status(200).json(result.rows);
+    })
+    .catch((error) => {
+      response.status(400).json(error);
+    });
+});
+
+app.listen(port, function () {
+  console.log(`Server is listening on port ${port}. Ready to accept requests!`);
 });
 
 module.exports = app;
