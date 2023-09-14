99eb26bdc0920f6999eb03bdb56496dec68f3315
christina-mifsud <christina.mifsud@gmail.com>
2023-09-14 20:33:40 +0100

    Include db credentials

--- E-Commerce-API/app.js
+++ E-Commerce-API/app.js
@@ -5,4 +5,14 @@ const app = express();
 // Use environment variables instead:
 // https://www.codementor.io/@parthibakumarmurugesan/what-is-env-how-to-set-up-and-run-a-env-file-in-node-1pnyxw9yxj
 
+const { Pool } = require("pg");
+
+const db = new Pool({
+  user: process.env.DB_USERNAME,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
 module.exports = app;
