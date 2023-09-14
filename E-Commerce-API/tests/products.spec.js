9ab3ccef34431942801af26cc01dd108b1592d4a
christina-mifsud <christina.mifsud@gmail.com>
2023-09-14 21:02:52 +0100

    User story 1 - test and endpoint

--- E-Commerce-API/tests/products.spec.js
+++ E-Commerce-API/tests/products.spec.js
@@ -1,6 +1,7 @@
 const request = require("supertest");
 const app = require("../app");
 
+// 1. As a user, I want to view a list of all products with their prices and supplier names.
 describe("GET /products", () => {
   it("should return a list of all product names with their prices and supplier names", async () => {
     const response = await request(app).get("/products");
