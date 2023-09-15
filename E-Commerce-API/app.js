435f749a25a32e6e1c53be888ef836eec7839b0b
Irianni Munoz <82213740+munozirianni1988@users.noreply.github.com>
2023-09-15 21:56:06 +0100

    configuring server
    creating global variables and configuring server

--- E-Commerce-API/app.js
+++ E-Commerce-API/app.js
@@ -1,8 +1,22 @@
-const express = require("express");
-const app = express();
 // Your code to run the server should go here
 // Don't hardcode your DB password in the code or upload it to GitHub! Never ever do this ever.
 // Use environment variables instead:
 // https://www.codementor.io/@parthibakumarmurugesan/what-is-env-how-to-set-up-and-run-a-env-file-in-node-1pnyxw9yxj
 
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const port = process.env.DB_PORT || 5000;
+const { Pool } = require("pg");
+app.use(express.json());
+
 module.exports = app;
+
+const db = new Pool({
+  user: process.env.DB_USERNAME,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: String(process.env.DB_PASSWORD),
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
