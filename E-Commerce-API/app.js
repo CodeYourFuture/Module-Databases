5e08cf79b76bd23fbe0a920d5137b19aad26fab7
Irianni Munoz <82213740+munozirianni1988@users.noreply.github.com>
2023-09-16 11:44:23 +0100

    Update app.js

--- E-Commerce-API/app.js
+++ E-Commerce-API/app.js
@@ -6,7 +6,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const port = process.env.DB_PORT || 5000;
+const port = process.env.PORT || 5000;
 const { Pool } = require("pg");
 app.use(express.json());
 
@@ -17,6 +17,20 @@ const db = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: String(process.env.DB_PASSWORD),
+  port: process.env.DB_PORT,
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.get("/products", (req, res) => {
+  db.query(
+    'SELECT p.product_name AS name, pa.unit_price AS price, s.supplier_name AS "supplierName" FROM products p JOIN product_availability pa ON (p.id = pa.prod_id) JOIN suppliers s ON ( pa.supp_id = s.id)'
+  )
+    .then((result) => {
+      console.log(result.rows);
+      res.status(200).json(result.rows);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
