1caaaf9b25bded7bbfdbb9d91326b1442bd7f99a
bahador <noor.bahadory786@gmail.com>
2024-05-12 19:40:18 +0100

    completed post product endpoint

--- E-Commerce-API/app.js
+++ E-Commerce-API/app.js
@@ -1,8 +1,104 @@
 const express = require("express");
 const app = express();
-// Your code to run the server should go here
-// Don't hardcode your DB password in the code or upload it to GitHub! Never ever do this ever.
-// Use environment variables instead:
-// https://www.codementor.io/@parthibakumarmurugesan/what-is-env-how-to-set-up-and-run-a-env-file-in-node-1pnyxw9yxj
-
+//require("dotenv").config();
+const port = 3307;
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+const pg = require("pg");
 module.exports = app;
+
+const { Pool } = pg;
+const db = new Pool({
+  user: "Damoon",
+  host: "localhost",
+  database: "cyf_ecommerce",
+  password: "",
+  port: 5432,
+});
+
+app.get("/products", async (req, res) => {
+  try {
+    const result = await db.query(
+      `SELECT pro.product_name, sup.supplier_name, pa.unit_price
+        FROM products pro JOIN suppliers sup ON pro.id = sup.id
+        JOIN product_availability pa ON pa.supp_id = sup.id `
+    );
+
+    res.send(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+//get by name
+app.get("/products/:name", (req, res) => {
+  let productName = req.params.name;
+  productName = productName.replace(/-/g, " ");
+  db.query("SELECT * FROM products WHERE lower(product_name) LIKE $1 || '%'", [
+    productName.toLowerCase(),
+  ]).then((result) => res.json(result.rows));
+});
+
+//get by id
+app.get("/customers/:id", function (req, res) {
+  const custId = parseInt(req.params.id);
+  db.query("SELECT * FROM customers WHERE id = $1", [custId])
+    .then((result) => {
+      res.status(200).json(result.rows);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        error: {
+          message: "internal server error",
+          type: "database error",
+        },
+      });
+    });
+});
+
+//post customers
+app.post("/customers", (req, res) => {
+  const name = req.body.name;
+  const address = req.body.address;
+  const city = req.body.city;
+  const country = req.body.country;
+  const query = `INSERT INTO customers(name, address, city, country)VALUES($1,$2,$3,$4)`;
+  db.query(query, [name, address, city, country])
+    .then(() => {
+      res.status(200).send("created a new customer was successfully !");
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        error: {
+          message: "internal server error",
+          type: "database error",
+        },
+      });
+    });
+});
+
+//post product
+app.post("/products", (req, res) => {
+  const productName = req.body.product_name;
+  const query = `INSERT INTO products(product_name)VALUES($1)`;
+  db.query(query, [productName])
+    .then(() => {
+      res.status(200).send("created a new product was successfully !");
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        error: {
+          message: "internal server error",
+          type: "database error",
+        },
+      });
+    });
+});
+
+app.listen(port, () => {
+  console.log(`server listening on port ${port} !`);
+});
--- E-Commerce-API/tests/products.spec.js
+++ E-Commerce-API/tests/products.spec.js
@@ -2,17 +2,64 @@ const request = require("supertest");
 const app = require("../app");
 
 describe("GET /products", () => {
-  it("should return a list of all product names with their prices and supplier names", async () => {
-    const response = await request(app).get("/products");
+  it("should return a list of all product ", async () => {
+    const product = "Javascript Book";
+    const response = await request(app).get(`/products ${product}`);
     expect(response.status).toBe(200);
-    expect(response.body).toEqual(
+    expect(response.body) ===
       expect.arrayContaining([
         expect.objectContaining({
           name: expect.any(String),
           price: expect.any(Number),
           supplierName: expect.any(String),
         }),
-      ])
-    );
+      ]);
+  });
+});
+
+describe("GET /products/:name", () => {
+  it("should return product details for the specified name", async () => {
+    const productName = [
+      {
+        id: 1,
+        name: "karam",
+        address: "770-2839 Ligula Road",
+        city: "Birmingham",
+        country: "UK",
+      },
+      {
+        id: 2,
+        name: "marcus",
+        address: "770-2839 Ligula new",
+        city: "Birmingham",
+        country: "UK",
+      },
+    ];
+    const response = await request(app).get(`/products/ ${productName}`);
+    expect(response.status).toBe(200);
+    expect(response.body) === expect.any(Array);
+    response.body.forEach((product) => {
+      expect(product).toEqual(
+        expect.objectContaining({
+          product_name: expect.any(String),
+        })
+      );
+    });
+  });
+});
+
+describe("GET /customers/:id", () => {
+  it("should load a single customer by their ID", async () => {
+    const customerId = 3;
+    const response = await request(app).get(`/customers/${customerId}`);
+    expect(response.status).toBe(200);
+    expect(response.body) === expect.any(Array);
+    response.body.forEach((product) => {
+      expect(product).toEqual(
+        expect.objectContaining({
+          customer_id: expect.any(Number),
+        })
+      );
+    });
   });
 });
