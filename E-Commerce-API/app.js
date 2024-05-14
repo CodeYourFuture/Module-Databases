e58240b07c8b1d9ff3c60b4651a73fd4d1e4b673
karimi <behrouzkarimi1993@gmail.com>
2024-05-14 14:09:55 +0100

    want to view a single customer by their ID , tests passed

--- E-Commerce-API/app.js
+++ E-Commerce-API/app.js
@@ -32,16 +32,21 @@ const db = new Pool({
   port: 5432,
 });
 
+//This endpoint returns all the products with name price and suppliername
+//If user search for an item using query parameter it return the item if available
 app.get("/products", async (req, res) => {
   const queryParam = req.query.search;
-  // console.log(queryParam, queryParam.length, "<-------queryparam");
+
   try {
+    //If no query parametre defined by the user
     if (queryParam === undefined) {
       const products = await db.query(
         "SELECT pr.product_name AS NAME , pa.unit_price AS price, sp.supplier_name AS supplierName FROM product_availability pa JOIN suppliers sp ON(pa.supp_id=sp.id) JOIN products pr ON (pa.prod_id=pr.id);"
       );
       res.status(200).send(products.rows);
-    } else {
+    }
+    //If user search for a specific item
+    else {
       const queriedSearch = await db.query(
         "SELECT pr.product_name AS NAME , pa.unit_price AS price, sp.supplier_name AS supplierName FROM product_availability pa JOIN suppliers sp ON(pa.supp_id=sp.id) JOIN products pr ON (pa.prod_id=pr.id) WHERE pr.product_name=$1;",
         [queryParam]
@@ -57,6 +62,25 @@ app.get("/products", async (req, res) => {
   } catch (err) {
     console.error(err, "<----Error happened");
     res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.get("/customers/:customerId", async (req, res) => {
+  const idOfCustomer = req.params.customerId;
+  console.log(idOfCustomer, "id of customer");
+  try {
+    const foundCustomer = await db.query(
+      "SELECT * FROM customers WHERE id=$1",
+      [idOfCustomer]
+    );
+    if (foundCustomer.rows.length !== 0) {
+      res.status(200).json(foundCustomer.rows);
+    } else {
+      res.status(404).json({ message: "Customer with the id -1 not found!" });
+    }
+  } catch (error) {
+    console.error("Error: ", error);
+    res.status(500).json({ error: "Internal server error!" });
   }
 });
 
--- E-Commerce-API/tests/products.spec.js
+++ E-Commerce-API/tests/products.spec.js
@@ -44,3 +44,31 @@ describe("GET /products?search=existenceItem and nonexistenceItem", () => {
     );
   });
 });
+
+describe("GET /customers/:customerId , should load a single customer by their ID.", () => {
+  it("should resturn a specific customer with determined id", async () => {
+    const response = await request(app).get("/customers/1");
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: expect.any(Number),
+          name: expect.any(String),
+          address: expect.any(String),
+          city: expect.any(String),
+          country: expect.any(String),
+        }),
+      ])
+    );
+  });
+
+  it("should return an object with a message if customer not found", async () => {
+    const response = await request(app).get("/customers/-1");
+    expect(response.status).toBe(404);
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        message: "Customer with the id -1 not found!",
+      })
+    );
+  });
+});
