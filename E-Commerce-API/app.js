29468d1a274cfefa68d00518ff48b97b942f3419
Pedro Ricciardi <riccip@outlook.com>
2024-05-29 09:48:40 +0100

    db access test success

--- E-Commerce-API/app.js
+++ E-Commerce-API/app.js
@@ -1,13 +1,20 @@
 const express = require("express");
-const { db } = require("./db");
+const db = require("./db");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", async (req, res) => {
+  // db access test
+  try {
+    const result = await db.query("SELECT * FROM customers");
+    console.log(req.body);
+    res.json(result.rows);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 app.listen(port, () => {
