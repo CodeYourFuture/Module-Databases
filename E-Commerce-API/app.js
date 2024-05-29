0561393000c59f55a3cd6594735205ae12addc13
Pedro Ricciardi <riccip@outlook.com>
2024-05-29 09:42:30 +0100

    port added as a variable

--- E-Commerce-API/app.js
+++ E-Commerce-API/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { db } = require("./db");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json())
 
@@ -9,8 +10,8 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
 
 
